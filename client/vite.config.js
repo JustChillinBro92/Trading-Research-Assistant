bc352701ab044af43c50d3145066ff4c6727044b
JustChillinBro92 <ayanpurkait11@gmail.com>
2026-09-14 02:21:49 +0530

    -> Vite config fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  root: ".",
-  plugins: [react()],
-  server: { port: 5173, proxy: { '/api': 'http://localhost:3001' } }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    root: ".",
+    plugins: [react()],
+    define: {
+      "import.meta.env.BACKEND_API_URL": JSON.stringify(
+        env.BACKEND_API_URL || "",
+      ),
+    },
+    server: { port: 5173, proxy: { "/api": "http://localhost:3001" } },
+  };
 });
